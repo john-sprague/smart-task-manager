5a2580526ad33bfeb64c9ba4dab7479809677228
John Sprague <spraguejdev@gmail.com>
2026-03-20 02:49:31 -0500

    style: add header layout styling to homepage

--- src/App.jsx
+++ src/App.jsx
@@ -5,8 +5,9 @@ function App() {
 
   return (
     <>
-
-
+      <div className="mx-auto max-w-xl mt-10 text-center" >
+        <h1 className="text-2xl font-bold mb-4">Smart Task Manager</h1>
+      </div>
     </>
   )
 }
