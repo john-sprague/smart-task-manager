50d70ab9c47520270672e290f287f3dacbf9a250
John Sprague <spraguejdev@gmail.com>
2026-04-05 22:10:10 -0500

    chore: complete useDebounce hook

---
+++ src/hooks/useDebounce.tsx
@@ -0,0 +1,13 @@
+import { useState, useEffect } from "react";
+
+const useDebounce = (value: string, delay: number = 300): string => {
+  const [debounced, setDebounced] = useState<string>(value);
+
+  useEffect(() => {
+    const id = setTimeout(() => setDebounced(value), delay);
+    return () => clearTimeout(id);
+  }, [value, delay]);
+  return debounced;
+};
+
+export default useDebounce;
